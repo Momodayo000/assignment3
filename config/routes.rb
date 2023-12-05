64cc1d46f74feaf87663ecf553a8056350b038bf
Momodayo000 <momodayo000net@gmail.com>
2023-12-05 11:52:52 +0000

    [add] routes favorites

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/home/about" => "homes#about"
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
