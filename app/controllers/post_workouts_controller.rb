f4dfb376e082f60f28e158074b02ba0567cdd09a
Momodayo000 <momodayo000net@gmail.com>
2023-12-14 09:28:49 +0000

    [add] post workouts controller

---
+++ app/controllers/post_workouts_controller.rb
@@ -0,0 +1,14 @@
+class Public::PostWorkoutsController < ApplicationController
+  def create
+    @post_workout = PostWorkout.new(post_workout_params)
+    @post_workout.end_user_id = current_end_user.id
+     # 受け取った値を,で区切って配列にする
+    tag_list = params[:post_workout][:name].split(',')
+    if @post_workout.save
+      @post_workout.save_workout_tags(tag_list)
+      redirect_to post_workouts_path, notice:'投稿が完了しました'
+    else
+      render :new
+    end
+  end
+end
--- app/models/post_workout_tag.rb
+++ app/models/post_workout_tag.rb
@@ -1,4 +1,6 @@
 class PostWorkoutTag < ApplicationRecord
   belongs_to :post_workout
   belongs_to :workout_tag
+  
+  
 end
