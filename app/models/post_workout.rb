25fb152c3ff22ba66766ebb3e41bd96e514d60ac
Momodayo000 <momodayo000net@gmail.com>
2023-12-17 16:11:51 +0000

    model post workout

--- app/models/post_workout.rb
+++ app/models/post_workout.rb
@@ -2,6 +2,7 @@ class PostWorkout < ApplicationRecord
 
   def save_workout_tags(tags)
     current_tags = self.workout_tags.pluck(:name) unless self.workout_tags.nil?
+    old_tags = current_tags - tags
   end
 
 end
